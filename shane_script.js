e39693645ee586ae3a39105d139966e3aa2b78f4
Shane <31552294+Ploxly@users.noreply.github.com>
2020-01-09 21:03:45 -0500

    Organizational changes and formatting

--- shane_script.js
+++ shane_script.js
@@ -2,3 +2,13 @@ function animefunction(space) {
     space.classList.toggle("change");
     return;
   }
+
+
+window.onload = function wordfunction() {
+  var encourage_words = ["with the google search engine as a mentor", "Improvise. Adapt. Overcome.", "fall seven times, stand up eight.",
+  "harder, better, faster, and stronger.", "should've taken ''bio making 101.''"]
+
+  var random = Math.ceil(Math.random() * encourage_words.length - 1)
+
+  document.getElementById("encouraging_quote").innerText = encourage_words[random]
+};
