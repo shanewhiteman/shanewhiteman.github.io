f8da9bd11d60e5d105abcd061fb1af04b464866f
Shane <Shanewhiteman99@gmail.com>
2020-02-07 20:17:16 -0500

    New website functionality added
    Made it so the image changes the yellow highlighted words as well as loading the window

--- shane_script.js
+++ shane_script.js
@@ -1,8 +1,17 @@
-window.onload = function wordfunction() {
+function wordfunction() {
   var encourage_words = ["with the google search engine as a mentor", "Improvise. Adapt. Overcome.", "fall seven times, stand up eight.",
-  "harder, better, faster, and stronger.", "should've taken ''bio making 101.''"]
+  "harder, better, faster, and stronger.", "should've taken ''bio making 101.''","With a quick learning sense!"]
 
   var random = Math.ceil(Math.random() * encourage_words.length - 1)
 
+  document.getElementById("encouraging_quote").innerText = encourage_words[random]
+};
+
+window.onload = function onloadfunction() {
+  var encourage_words = ["with the google search engine as a mentor", "Improvise. Adapt. Overcome.", "fall seven times, stand up eight.",
+  "harder, better, faster, and stronger.", "should've taken ''bio making 101.''","With a quick learning sense!"]
+  
+  var random = Math.ceil(Math.random() * encourage_words.length - 1)
+  
   document.getElementById("encouraging_quote").innerText = encourage_words[random]
 };
