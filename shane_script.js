cc0ad775f3c4df909598e948c706702b4bb6942c
Shane <Shanewhiteman99@gmail.com>
2025-09-14 10:21:39 -0400

    Update shane_script.js

--- shane_script.js
+++ shane_script.js
@@ -1,6 +1,6 @@
 function wordfunction() {
-  var encourage_words = ["with the google search engine as a mentor", "Improvise. Adapt. Overcome.", "fall seven times, stand up eight.",
-  "harder, better, faster, and stronger.", "should've taken ''bio making 101.''","With a quick learning sense!"]
+  var encourage_words = ["running on caffeine and curiosity", "making stuff, breaking stuff, learning everything in between", "debugging life one problem at a time",
+  "breaking things to fix them better", "troubleshooting by day, gravity testing by night"]
 
   var random = Math.ceil(Math.random() * encourage_words.length - 1)
 
@@ -15,4 +15,5 @@ window.onload = function onloadfunction() {
   
   document.getElementById("encouraging_quote").innerText = encourage_words[random]
 };
+
 
